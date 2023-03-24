77d435293f62fd14188fba4026b6f108102ea964
andiniseptia <andiniseptiandri124@gmail.com>
2023-03-24 16:08:40 +0700

    Menampilkan npass

--- insertionsort2/insertionsort2.cpp
+++ insertionsort2/insertionsort2.cpp
@@ -65,7 +65,12 @@ void insertionsort() {
 			arr[j + 1] = arr[j];
 			j--;
 		}
+		arr[j + 1] = temp;
 
+		cout << "\nPass " << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
 	}
 
 }
