2668ed7500d5c71f22d4620071c571d36aa60440
andiniseptia <andiniseptiandri124@gmail.com>
2023-03-24 15:58:37 +0700

    Menambahkan data ke-

--- insertionsort2/insertionsort2.cpp
+++ insertionsort2/insertionsort2.cpp
@@ -43,6 +43,11 @@ void input() {
 	cout << "======================" << endl;
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
-
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 
 }
+
