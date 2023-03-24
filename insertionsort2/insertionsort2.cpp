94726cc8920897a0a25148ccee2206ff8a77749d
andiniseptia <andiniseptiandri124@gmail.com>
2023-03-24 16:09:28 +0700

    Menambahkan prosedur display

--- insertionsort2/insertionsort2.cpp
+++ insertionsort2/insertionsort2.cpp
@@ -74,3 +74,14 @@ void insertionsort() {
 	}
 
 }
+
+void display() {
+	cout << "\n================================" << endl;
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
+
