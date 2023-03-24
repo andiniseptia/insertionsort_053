df33ca5478c4abdb1b5ddab8f4e420048f21c862
andiniseptia <andiniseptiandri124@gmail.com>
2023-03-24 15:55:54 +0700

    Membuat prosedur input

--- insertionsort2/insertionsort2.cpp
+++ insertionsort2/insertionsort2.cpp
@@ -4,3 +4,18 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+	while (true) {
+		cout << "Masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+
+}
